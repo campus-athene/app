7e6aea23d71d9a085ad7f89a523fae28e79cd13f
OliverRM <git@OliverRM.de>
2020-02-11 15:37:31 +0800

    Added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container'
+import { Image, Alert, Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image src={logo} alt="logo" />
+      <Alert variant="primary">
+        Edit <code>src/App.js</code> and save to reload.
+      </Alert>
+      <Button
+        variant="primary"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </Button>
+    </Container>
   );
 }
 
