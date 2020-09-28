3ddc53a3ae5615e98cae8ab6514e63b9bafbdd5c
OliverRM <git@OliverRM.de>
2020-09-28 16:59:31 +0200

    Updated url for S1 map

--- src/features/maps/MapsPage.jsx
+++ src/features/maps/MapsPage.jsx
@@ -6,7 +6,7 @@ const MapsPage = () => {
   return (
     <PageFrame title="Karten">
       <h3 style={{ marginTop: "0.5em" }}>Stadtmitte Mitte (S1)</h3>
-      <Image thumbnail style={{ marginBottom: "1em" }} src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-detail_S1.png" />
+      <Image thumbnail style={{ marginBottom: "1em" }} src="https://www.tu-darmstadt.de/media/dezernat_iv/lageplnews0809/s1.jpg" />
       <h3>Stadtmitte Nord (S2)</h3>
       <Image thumbnail style={{ marginBottom: "1em" }} src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-detail_S2.png" />
       <h3>Stadtmitte Süd (S3)</h3>
