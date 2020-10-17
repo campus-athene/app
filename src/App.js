dac34738ba2ced394baa615dc9f5aef315b65ee8
OliverRM <git@oliverrm.de>
2020-10-17 12:56:26 +0200

    Cleaning up index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './redux';
-import { update } from './redux/sync';
+import React from 'react';
 import Router from './Router';
 import './App.css';
 
-const store = configureStore({
-  reducer: rootReducer
-});
-
 const App = () => {
-  // Check for outdated or missing state and fetch is asyncronously from the server.
-  useEffect(() => store.dispatch(update()), []);
-
   return (
-    <Provider store={store}>
-      <Router />
-    </Provider>
+    <Router />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk'
-import rootReducer from './redux';
 import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './redux';
+import { update } from './redux/sync';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware
-  ));
+const store = configureStore({
+  reducer: rootReducer,
+});
+// Check for outdated or missing state and fetch is asyncronously from the server.
+store.dispatch(update());
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'));
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
