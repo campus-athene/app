13393362ce53513dd98ce2cb8f422319273ec1c8
OliverRM <git@oliverrm.de>
2020-11-25 11:30:00 +0100

    New icons for HomeScreen

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -2,29 +2,28 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { Button, Modal } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faChalkboardTeacher, faFileAlt, faBook, faMapMarkedAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { logout } from '../auth/state';
 import PageFrame from '../common/PageFrame';
 import Logo from '../common/Logo';
+import { Envelope, Exam, Lecture, Logout, Map, Orientation } from '../../icons';
 
 const HomePage = ({ logout }) => {
   const [logoutModal, setLogoutModal] = useState();
   const history = useHistory();
 
-  const HomeButton = ({ target, onClick, icon, seperator, color, children }) =>
-    <h4 style={{ marginTop: seperator ? '1em' : '0.5em', color }} onClick={onClick || (() => history.push(target))}>
-      <FontAwesomeIcon icon={icon} style={{ width: '1.333em', marginRight: '0.5em' }} />{children}</h4>;
+  const HomeButton = ({ target, onClick, icon: Icon, seperator, color, children }) =>
+    <h4 style={{ marginTop: seperator ? '1.5em' : '0.5em', color }} onClick={onClick || (() => history.push(target))}>
+      <Icon style={{ display: 'inline-block', width: '1.875em', marginLeft: '0.125em', marginRight: '0.625em' }} />{children}</h4>;
 
   return (
     <PageFrame noBack>
-      <Logo style={{ textAlign: 'center', paddingTop: '2rem', height: '40vw' }} />
-      <HomeButton target={'/messages'} icon={faEnvelope}>Nachrichten</HomeButton>
-      <HomeButton target={'/classreg'} icon={faChalkboardTeacher}>Veranstaltungsanmeldung</HomeButton>
-      <HomeButton target={'/exams'} icon={faFileAlt}>Prüfungen</HomeButton>
-      <HomeButton target={'/oapp'} icon={faBook}>Orientierung</HomeButton>
-      <HomeButton target={'/maps'} icon={faMapMarkedAlt}>Campuskarten</HomeButton>
-      <HomeButton onClick={() => setLogoutModal(true)} icon={faSignOutAlt} seperator color="#dc3545">Abmelden</HomeButton>
+      <Logo style={{ textAlign: 'center', paddingTop: '2rem', height: '10rem' }} />
+      <HomeButton target={'/messages'} icon={Envelope}>Nachrichten</HomeButton>
+      <HomeButton target={'/classreg'} icon={Lecture}>Veranstaltungen</HomeButton>
+      <HomeButton target={'/exams'} icon={Exam}>Prüfungen</HomeButton>
+      <HomeButton target={'/oapp'} icon={Orientation}>Orientierung</HomeButton>
+      <HomeButton target={'/maps'} icon={Map}>Campuskarten</HomeButton>
+      <HomeButton onClick={() => setLogoutModal(true)} icon={Logout} seperator color="#dc3545">Abmelden</HomeButton>
       <Modal show={logoutModal} centered>
         <Modal.Header>
           <Modal.Title>Abmelden</Modal.Title>
