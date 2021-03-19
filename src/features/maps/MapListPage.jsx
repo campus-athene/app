b06632c991bf6ea613dbc5b6ebd38d4e5bd6b03c
OliverRM <git@oliverrm.de>
2021-03-19 15:28:45 +0100

    Reworked MapListPage

--- src/features/maps/MapListPage.jsx
+++ src/features/maps/MapListPage.jsx
@@ -1,52 +1,88 @@
 import React from 'react';
 import PageFrame from '../common/PageFrame';
-import { Image } from 'react-bootstrap';
 import { useHistory } from 'react-router';
+import stadtmitte from './data/stadtmitte.png';
+import lichtwiese from './data/lichtwiese.png';
+import windkanal from './data/windkanal.png';
+
+const MapPreview = ({ title, abbrev, onClick, map, viewBox }) => (
+  <div
+    style={{
+      marginBottom: '1em',
+      position: 'relative',
+    }}
+    onClick={onClick}
+  >
+    <svg viewBox={viewBox} style={{ borderRadius: '0.5rem' }}>
+      <image href={map} preserveAspectRatio="none" width="1000" />
+    </svg>
+    <div
+      style={{
+        position: 'absolute',
+        bottom: '0',
+        background: '#fffa',
+        right: 0,
+        left: 0,
+        fontSize: '1.2em',
+        padding: '0.25em 0.75em',
+      }}
+    >
+      <span style={{ fontWeight: 'bold' }}>{abbrev}</span>
+      {title}
+    </div>
+    <div
+      style={{
+        position: 'absolute',
+        borderRadius: '0.5em',
+        border: '1px solid lightgray',
+        top: '0',
+        right: '0',
+        bottom: '0',
+        left: '0',
+      }}
+    />
+  </div>
+);
 
 const MapListPage = () => {
   const history = useHistory();
   return (
     <PageFrame title="Karten">
-      <h3 style={{ marginTop: '0.5em' }}>Stadtmitte Mitte (S1)</h3>
-      <Image
-        thumbnail
-        style={{ marginBottom: '1em' }}
+      <div style={{ height: '1em' }} />
+      <MapPreview
+        title="tadtmitte"
+        abbrev="S"
         onClick={() => history.push('/maps/stadtmitte')}
-        src="https://www.tu-darmstadt.de/media/dezernat_iv/lageplnews0809/s1.jpg"
+        map={stadtmitte}
+        viewBox="490 330 414 207"
       />
-      <h3>Stadtmitte Nord (S2)</h3>
-      <Image
-        thumbnail
-        style={{ marginBottom: '1em' }}
-        onClick={() => history.push('/maps/stadtmitte')}
-        src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-detail_S2.png"
-      />
-      <h3>Stadtmitte Süd (S3)</h3>
-      <Image
-        thumbnail
-        style={{ marginBottom: '1em' }}
-        onClick={() => history.push('/maps/stadtmitte')}
-        src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-detail_S3_870x0.png"
-      />
-      <h3>Lichtwiese (L)</h3>
-      <Image
-        thumbnail
-        style={{ marginBottom: '1em' }}
+      <MapPreview
+        title="ichtwiese"
+        abbrev="L"
         onClick={() => history.push('/maps/lichtwiese')}
-        src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-detail_L_870x0.png"
+        map={lichtwiese}
+        viewBox="445 550 500 250"
       />
-      <h3>Botanischer Garten (B)</h3>
-      <Image
-        thumbnail
-        style={{ marginBottom: '1em' }}
-        onClick={() => history.push('/maps/lichtwiese')}
-        src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-detail_B_870x0.png"
+      <MapPreview
+        title="otanischer Garten"
+        abbrev="B"
+        onClick={() => history.push('/maps/botanischergarten')}
+        map={lichtwiese}
+        viewBox="470 70 500 250"
       />
-      <h3>Hochschulstadion (H)</h3>
-      <Image
-        thumbnail
-        style={{ marginBottom: '1em' }}
-        src="https://www.tu-darmstadt.de/media/daa_responsives_design/01_die_universitaet_medien/campus_medien_1/lageplaene_medien/karten_medien/lageplan-tu-overview_H_870x0.png"
+      <MapPreview
+        title="ochschulstadion"
+        abbrev="H"
+        onClick={() => history.push('/maps/hochschulstadion')}
+        map={lichtwiese}
+        viewBox="100 660 500 250"
+      />
+      <MapPreview
+        title="indkanal"
+        abbrev="W"
+        onClick={() => history.push('/maps/windkanal')}
+        map={windkanal}
+        viewBox="0 240 1000 500"
       />
     </PageFrame>
   );
