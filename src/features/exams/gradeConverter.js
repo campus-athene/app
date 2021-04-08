48c08fcc19ebdb46fce54ab67afd728c32463db1
OliverRM <git@oliverrm.de>
2021-04-08 22:17:45 +0200

    Corrected grade descriptions and colours

--- src/features/exams/gradeConverter.js
+++ src/features/exams/gradeConverter.js
@@ -2,14 +2,16 @@ const convertGrade = (grade) => {
   switch (grade) {
     case '1,0': return { desc: 'sehr gut', color: 'success' };
     case '1,3': return { desc: 'sehr gut', color: 'success' };
-    case '1,7': return { desc: 'sehr gut', color: 'success' };
+    case '1,7': return { desc: 'gut', color: 'success' };
     case '2,0': return { desc: 'gut', color: 'success' };
     case '2,3': return { desc: 'gut', color: 'success' };
-    case '2,7': return { desc: 'gut', color: 'success' };
+    case '2,7': return { desc: 'befriedigend', color: 'warning' };
     case '3,0': return { desc: 'befriedigend', color: 'warning' };
     case '3,3': return { desc: 'befriedigend', color: 'warning' };
-    case '3,7': return { desc: 'befriedigend', color: 'warning' };
+    case '3,7': return { desc: 'ausreichend', color: 'warning' };
     case '4,0': return { desc: 'ausreichend', color: 'warning' };
+    // Official is "nicht ausreichend" for Prüfungen and Studienleistungen
+    // and "nicht bestanden" for Module
     case '5,0': return { desc: 'ungenügend', color: 'danger' };
     case 'b': return { desc: 'bestanden', color: 'success' };
     case 'nb': return { desc: 'nicht bestanden', color: 'danger' };
