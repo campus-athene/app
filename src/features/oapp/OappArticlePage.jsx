b2aee8e4e47c40cb672f2c03354070bc88a23bb2
OliverRM <git@oliverrm.de>
2021-06-07 21:13:04 +0200

    Format oapp page code

--- src/features/oapp/OappArticlePage.jsx
+++ src/features/oapp/OappArticlePage.jsx
@@ -6,23 +6,43 @@ import articles from './articles.json';
 
 const OappArticlePage = () => {
   const params = useParams();
-  const url = params.l3 ? `/${params.l1}/${params.l2}/${params.l3}/` : `/${params.l1}/${params.l2}/`;
+  const url = params.l3
+    ? `/${params.l1}/${params.l2}/${params.l3}/`
+    : `/${params.l1}/${params.l2}/`;
   const a = articles[url];
-  return <PageFrame title={a.title1}>
-    {a.image ? <img src={'https://www.oapp.tu-darmstadt.de' + a.image} alt="" style={{ width: '100%' }} /> : null}
-    <h2 style={{ userSelect: 'text' }}>{a.title2}</h2>
-    {!a.section2 ?
-      <div style={{ userSelect: 'text' }} dangerouslySetInnerHTML={{ __html: a.section1 }} /> :
-      <Tabs defaultActiveKey="tab1">
-        <Tab title={a.tab1} eventKey="tab1">
-          <div style={{ userSelect: 'text' }} dangerouslySetInnerHTML={{ __html: a.section1 }} />
-        </Tab>
-        <Tab title={a.tab2} eventKey="tab2">
-          <div style={{ userSelect: 'text' }} dangerouslySetInnerHTML={{ __html: a.section2 }} />
-        </Tab>
-      </Tabs>
-    }
-  </PageFrame>
+  return (
+    <PageFrame title={a.title1}>
+      {a.image ? (
+        <img
+          src={'https://www.oapp.tu-darmstadt.de' + a.image}
+          alt=""
+          style={{ width: '100%' }}
+        />
+      ) : null}
+      <h2 style={{ userSelect: 'text' }}>{a.title2}</h2>
+      {!a.section2 ? (
+        <div
+          style={{ userSelect: 'text' }}
+          dangerouslySetInnerHTML={{ __html: a.section1 }}
+        />
+      ) : (
+        <Tabs defaultActiveKey="tab1">
+          <Tab title={a.tab1} eventKey="tab1">
+            <div
+              style={{ userSelect: 'text' }}
+              dangerouslySetInnerHTML={{ __html: a.section1 }}
+            />
+          </Tab>
+          <Tab title={a.tab2} eventKey="tab2">
+            <div
+              style={{ userSelect: 'text' }}
+              dangerouslySetInnerHTML={{ __html: a.section2 }}
+            />
+          </Tab>
+        </Tabs>
+      )}
+    </PageFrame>
+  );
 };
 
 export default OappArticlePage;
--- src/features/oapp/OappPage.jsx
+++ src/features/oapp/OappPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Accordion } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { faBook, faComments, faExternalLinkAlt, faHandsHelping, faRunning } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBook,
+  faComments,
+  faExternalLinkAlt,
+  faHandsHelping,
+  faRunning,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PageFrame from '../common/PageFrame';
 import tree from './menutree.json';
@@ -12,10 +18,10 @@ const idToIcon = (id) => {
   if (id === 'soziales') return faHandsHelping;
   if (id === 'kultur-freizeit-kontakte') return faRunning; // faBiking;
   if (id === 'beratung') return faComments;
-}
+};
 
 const getStyle = (level) => {
-  return ({
+  return {
     position: 'relative',
     display: 'flex',
     padding: '0.75rem 1.25rem',
@@ -24,25 +30,45 @@ const getStyle = (level) => {
     backgroundColor: level === 1 ? 'rgba(0, 0, 0, 0.03)' : '#fff',
     border: '1px solid rgba(0, 0, 0, 0.125)',
     borderWidth: '1px 0',
-    alignItems: 'center'
-  });
-}
+    alignItems: 'center',
+  };
+};
 
 const LinkItem = ({ item, level, ...props }) => {
-  const type = !item.href || item.href === '#' ? 0 : item.external ? 2 : item.href[0] === '/' ? 1 : 0;
+  const type =
+    !item.href || item.href === '#'
+      ? 0
+      : item.external
+      ? 2
+      : item.href[0] === '/'
+      ? 1
+      : 0;
   const Element = ['a', Link, 'a'][type];
-  return <Element
-    key={item.id}
-    to={type === 1 ? '/oapp' + item.href : undefined}
-    href={type === 2 ? item.href : undefined}
-    target={type === 2 ? 'blank' : undefined}
-    style={getStyle(level)}
-    {...props}>
-    {level === 1 && idToIcon(item.id) ? <FontAwesomeIcon icon={idToIcon(item.id)} style={{ minWidth: '1.5em', marginRight: '0.4em' }} /> : null}
-    <div style={{ flexGrow: '1' }}>{item.title}</div>
-    {type === 2 ? <FontAwesomeIcon icon={faExternalLinkAlt} style={{ marginLeft: '0.4em' }} /> : null}
-  </Element>;
-}
+  return (
+    <Element
+      key={item.id}
+      to={type === 1 ? '/oapp' + item.href : undefined}
+      href={type === 2 ? item.href : undefined}
+      target={type === 2 ? 'blank' : undefined}
+      style={getStyle(level)}
+      {...props}
+    >
+      {level === 1 && idToIcon(item.id) ? (
+        <FontAwesomeIcon
+          icon={idToIcon(item.id)}
+          style={{ minWidth: '1.5em', marginRight: '0.4em' }}
+        />
+      ) : null}
+      <div style={{ flexGrow: '1' }}>{item.title}</div>
+      {type === 2 ? (
+        <FontAwesomeIcon
+          icon={faExternalLinkAlt}
+          style={{ marginLeft: '0.4em' }}
+        />
+      ) : null}
+    </Element>
+  );
+};
 
 const OappPage = () => {
   const [activeKey, setActiveKey] = useState([tree[0].id]);
@@ -51,33 +77,52 @@ const OappPage = () => {
 
   return (
     <PageFrame title="Orientierung">
-      <Accordion activeKey={getKey(1)} style={{ marginLeft: '-15px', marginRight: '-15px' }} onSelect={(key) => setKey(1, key)}>
-        {tree.map(l1 =>
+      <Accordion
+        activeKey={getKey(1)}
+        style={{ marginLeft: '-15px', marginRight: '-15px' }}
+        onSelect={(key) => setKey(1, key)}
+      >
+        {tree.map((l1) => (
           <div key={l1.id}>
-            <Accordion.Toggle as={LinkItem} item={l1} level={1} eventKey={l1.id} />
+            <Accordion.Toggle
+              as={LinkItem}
+              item={l1}
+              level={1}
+              eventKey={l1.id}
+            />
             <Accordion.Collapse eventKey={l1.id}>
-              <Accordion activeKey={getKey(2)} onSelect={(key) => setKey(2, key)}>
-                {l1.items.map(l2 =>
-                  l2.href ?
-                    <LinkItem key={l2.id} item={l2} level={2} /> :
+              <Accordion
+                activeKey={getKey(2)}
+                onSelect={(key) => setKey(2, key)}
+              >
+                {l1.items.map((l2) =>
+                  l2.href ? (
+                    <LinkItem key={l2.id} item={l2} level={2} />
+                  ) : (
                     <div key={l2.id}>
-                      <Accordion.Toggle as={LinkItem} item={l2} level={2} eventKey={l2.id} />
+                      <Accordion.Toggle
+                        as={LinkItem}
+                        item={l2}
+                        level={2}
+                        eventKey={l2.id}
+                      />
                       <Accordion.Collapse eventKey={l2.id}>
                         <>
-                          {l2.items.map(l3 =>
+                          {l2.items.map((l3) => (
                             <LinkItem key={l3.id} item={l3} level={3} />
-                          )}
+                          ))}
                         </>
                       </Accordion.Collapse>
                     </div>
+                  )
                 )}
               </Accordion>
             </Accordion.Collapse>
           </div>
-        )}
+        ))}
       </Accordion>
     </PageFrame>
   );
-}
+};
 
 export default OappPage;
