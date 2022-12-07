505596f9b8797a67b123c57a79fa1b283c4d0a68
Oliver Mayer <git@oliverrm.de>
2022-12-07 21:03:18 +0700

    Fix calendar showing in wrong timezone

--- src/features/calendar/calendarApi.ts
+++ src/features/calendar/calendarApi.ts
@@ -1,37 +1,38 @@
-import moment from 'moment';
+import moment, { utc } from 'moment';
 import mainApi from '../../provider/tucan/tucanApi';
 import { Appointment } from '../../provider/tucan/tucanApiTypes';
 import { RootState } from '../../redux';
+import { selectCreds } from '../auth/authSlice';
 
-export const selectAppointmentsOnDay = (day: number) => (state: any) => {
-  const from = day;
-  const to = moment(day).add(1, 'day');
-  return mainApi
-    .useAppointmentsQuery(null)
-    .data?.filter(
+const selectAllAppointments =
+  () =>
+  (state: RootState): Appointment[] => {
+    const creds = selectCreds()(state);
+    if (!creds) return [];
+    return mainApi.useAppointmentsQuery(null).data || [];
+  };
+
+export const selectAppointmentsOnDay =
+  (day: number) =>
+  (state: RootState): Appointment[] => {
+    const from = day;
+    const to = utc(day).add(1, 'day');
+    return selectAllAppointments()(state).filter(
       (a) =>
         moment(a.timeStart).utcOffset(0, true).diff(to) < 0 &&
         moment(a.timeEnd).utcOffset(0, true).diff(from) >= 0
     );
-};
+  };
 
-export const selectNextDayWithAppointments = () => (state: RootState) => {
-  const today = moment(Date.now()).tz('Europe/Berlin').startOf('day');
-  const data = mainApi.useAppointmentsQuery(null).data;
-  if (!data) return null;
-
-  return Math.min(
-    ...data.map((a) => today.diff(today, 'days')).filter((d) => d >= 0)
-  );
-};
-
-export const selectNextAppointment = () => (state: RootState) => {
-  const now = Date.now();
-  return mainApi.useAppointmentsQuery(null).data?.reduce((p, c) => {
-    if (new Date(c.timeEnd).getTime() < now) return p;
-    if (!p) return c;
-    return new Date(c.timeStart).getTime() < new Date(p.timeStart).getTime()
-      ? c
-      : p;
-  }, null as Appointment | null);
-};
+export const selectNextAppointment =
+  () =>
+  (state: RootState): Appointment | null => {
+    const now = Date.now();
+    return selectAllAppointments()(state).reduce<Appointment | null>((p, c) => {
+      if (new Date(c.timeEnd).getTime() < now) return p;
+      if (!p) return c;
+      return new Date(c.timeStart).getTime() < new Date(p.timeStart).getTime()
+        ? c
+        : p;
+    }, null);
+  };
